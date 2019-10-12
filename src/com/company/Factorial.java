f9092f974ff6f75bd9d6ef71831741645487387a
diiir <diiir727@gmail.com>
2019-10-12 18:37:22 +0300

    Добавили бесконечный повтор программы

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -6,19 +6,25 @@ public class Factorial {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Введите число");
-        int n = new Scanner(System.in).nextInt();
-        if (n < 0) {
 
-            System.out.println("Вы ввели отрицательное число");
-        } else if (n > 15) {
-            System.out.println("Вы ввели очень большое число");
-        } else {
-            int result = 1;
-            for (int i = 2; i <= n; i++) {
-                result = result * i;
+        int n;
+        System.out.println("Введите число");
+        String text = new Scanner(System.in).next();
+        while (!text.equals("q")){
+            n = Integer.parseInt(text);
+            if (n < 0) {
+                System.out.println("Вы ввели отрицательное число");
+            } else if (n > 15) {
+                System.out.println("Вы ввели очень большое число");
+            } else {
+                int result = 1;
+                for (int i = 2; i <= n; i++) {
+                    result = result * i;
+                }
+                System.out.println(n + "! =" + result);
             }
-            System.out.println(n + "! =" + result);
+            System.out.println("Введите новое число");
+            text = new Scanner(System.in).next();
         }
     }
 }
